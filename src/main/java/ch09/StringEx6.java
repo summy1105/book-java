aa4096000cea8f1eff3f0e7d4c35e2103a10e2ce
summy1105 <dsm851105@gmail.com>
2022-12-12 23:23:49 +0900

    ch 9 string

---
+++ src/main/java/ch09/StringEx6.java
@@ -0,0 +1,18 @@
+package ch09;
+
+public class StringEx6 {
+    public static void main(String[] args) {
+        int iVal = 100;
+        String strVal = String.valueOf(iVal);
+
+        double dVal = 200.0;
+        String strVal2 = dVal + "";
+
+        double sum = Integer.parseInt("+"+strVal) + Double.valueOf(strVal2);
+
+        double sum2 = Integer.valueOf(strVal.trim()) + Double.valueOf(strVal2.trim());
+
+        System.out.println(String.join("",strVal, "+", strVal2,"=")+sum);
+        System.out.println(strVal+"+"+strVal2+"="+sum2);
+    }
+}
