29dcfbf1c885a8bfdfc1e78c7412c0bfa284451c
summy1105 <dsm851105@gmail.com>
2022-12-27 23:20:11 +0900

    ch09 wrapper&objects

---
+++ src/main/java/ch09/ObjectsTest.java
@@ -0,0 +1,38 @@
+package ch09;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Objects;
+
+public class ObjectsTest {
+    public static void main(String[] args) {
+        String[][] str2D = new String[][]{{"aaa","bbb"},{"AAA","BBB"}};
+        String[][] str2D_2 = new String[][]{{"aaa","bbb"},{"AAA","BBB"}};
+
+        System.out.println("str2D = {");
+        for (String[] tmp : str2D){
+            System.out.println(Arrays.toString(tmp));
+        }
+        System.out.println("}\n");
+
+        System.out.println("str2D_2 = {");
+        for (String[] tmp : str2D_2){
+            System.out.println(Arrays.toString(tmp));
+        }
+        System.out.println("}\n");
+
+        System.out.println(Objects.equals(str2D, str2D_2));
+        System.out.println(Objects.deepEquals(str2D, str2D_2));
+        System.out.println(Objects.isNull(null));
+        System.out.println(Objects.nonNull(null));
+        System.out.println(Objects.hashCode(null));
+        System.out.println(Objects.toString(null));
+        System.out.println(Objects.toString(null, ""));
+        System.out.println();
+
+        Comparator c = String.CASE_INSENSITIVE_ORDER; // 대소문자 구분 안하는 comparator
+        System.out.println(Objects.compare("aa", "bb", c));
+        System.out.println(Objects.compare("bb", "aa", c));
+        System.out.println(Objects.compare("ab", "AB", c));
+    }
+}
---
+++ src/main/java/ch09/WrapperEx3.java
@@ -0,0 +1,30 @@
+package ch09;
+
+public class WrapperEx3 {
+    public static void main(String[] args) {
+        int i = 10;
+
+        Integer intg = (Integer) i;
+        Object obj = (Object) i;
+
+        Long lng = 100L;
+        int i2 = intg + 10;
+        long l = intg + lng;
+
+        Integer intg2 = new Integer(20);
+        int i3 = (int)intg2;
+        Integer intg3 = intg2 + i3;
+
+        System.out.println(i);
+        System.out.println(intg);
+        System.out.println(obj);
+        System.out.println();
+        System.out.println(lng);
+        System.out.println(i2);
+        System.out.println(l);
+        System.out.println();
+        System.out.println(intg2);
+        System.out.println(i3);
+        System.out.println(intg3);
+    }
+}
