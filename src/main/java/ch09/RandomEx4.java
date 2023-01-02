6b4baa88f5424f4a38d4d9d639a5db7ac71a2e1e
summy1105 <dsm851105@gmail.com>
2023-01-02 23:29:21 +0900

    ch09 random

---
+++ src/main/java/ch09/RandomEx4.java
@@ -0,0 +1,26 @@
+package ch09;
+
+public class RandomEx4 {
+    final static int RECORD_NUM = 10;
+    final static String TABLE_NAME = "TEST_TABLE";
+    final static String[] CODE1 = {"010", "011", "017", "018", "019"};
+    final static String[] CODE2 = {"Xy", "XX"};
+    final static String[] CODE3 = {"10", "20", "30", "40", "50"};
+
+    public static void main(String[] args) {
+        for (int i = 0; i < RECORD_NUM; i++) {
+            System.out.printf("INSERT INTO %s VALUES ('%s', '%s', '%s', %d);\n"
+                    , TABLE_NAME, getRandArr(CODE1), getRandArr(CODE2), getRandArr(CODE3), getRand(100, 200));
+        }
+    }
+
+    public static String getRandArr(String[] arr){
+        return arr[getRand(arr.length-1)];
+    }
+
+    public static int getRand(int n){ return getRand(0, n); }
+
+    public static int getRand(int from, int to){
+        return (int)(Math.random() * (Math.abs(to-from)+1))+Math.min(from, to);
+    }
+}
