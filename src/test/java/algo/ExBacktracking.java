5e193e78bca5b31149ab56c88819b377a5e1375d
summy1105 <73230224+summy1105@users.noreply.github.com>
2024-12-24 00:41:14 +0900

    backtracking & simulation 복습

--- src/test/java/algo/ExBacktracking.java
+++ src/test/java/algo/ExBacktracking.java
@@ -1,6 +1,11 @@
 package algo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  Back Tracking 개념
@@ -8,6 +13,9 @@ import org.junit.jupiter.api.Test;
     > for로는 확인 불가한 경우(깊이가 달라질 때)
     > 예 : 순열 - M개의 수에서 N개를 뽑아서, 뽑는 순서(!)의 경우의 갯수
  - depth에 재귀함수 사용
+ - 첫번째 고려할 알고리즘 DFS -> (재귀 혹은 스택)
+ - 트리의 깊이가 무한대가 될 때(미로찾기에서 루프가 있는 경우등), dfs보다 bfs가 낫다
+ - 최단거리는 BFS로
  */
 
 // 1 아이디어 : 어떤 알고리즘?
@@ -15,51 +23,94 @@ import org.junit.jupiter.api.Test;
 // 3 자료구조
 
 // 시간 복잡도에서 숫자가 크다, 보통 2억개 계산안에 해야 하기 때문에,
-// 중복이 허용된다면 N은 8까지 가능하고
-// 중복이 안된다면 N은 10까지 가능하다
+// 중복이 허용된다면 N은 8까지 가능하고 8^8 = 약 1700만 , 9^9 = 약3.9억
+// 중복이 안된다면 N은 10까지 가능하다? 11! = 약4000만
 
 // 백트래킹 문제는 N이 작음
 // 재귀함수 사용할 때, 종료시점에 자료구조 복구 잊지 말기
 
-// 백준 15649
+// 백준 https://www.acmicpc.net/problem/15649
 // 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
-//  - 1부터 N까지 자연수 중에서 중복 없이, M개를 고른 수열
+//  - 1부터 N까지 자연수 중에서 중복 없이, M개를 고른 수
+//    1 ≤ M ≤ N ≤ 8
 // 예제 input: "4 2"
 //      output : "1 2 \n 1 3 \n 1 4 \n 2 1 \n 2 3..."
 public class ExBacktracking {
-    int n;
-    int m;
+
     @Test
-    public void test() {
-        n = 10;
-        m = 4;
-        boolean[] visitCheck = new boolean[n+1];
-        int[] print = new int[m];
-
-        for (int i = 1; i <= n; i++) {
-            visitCheck[i] = true;
-            print[0] = i;
-            backtracking( 1, print, visitCheck);
-            visitCheck[i] = false;
-        }
-    }
-
-    void backtracking(int depth, int[] print, boolean[] visitCheck) {
-        if (depth == m) {
-            for (int i : print) {
+    public void 백준_예제1() {
+        int n = 3;
+        int m = 1;
+        List<int[]> list = execute(n, m);
+        for (int[] ints : list) {
+            for (int i : ints) {
                 System.out.printf("%d ", i);
             }
             System.out.println();
-            return;
         }
-        for (int i = 1; i <= n ; i++) {
-            if (visitCheck[i] == false) {
-                print[depth] = i;
-                visitCheck[i] = true;
-                backtracking(depth+1, print, visitCheck);
-                visitCheck[i] = false;
-            }
-        }
+        Assertions.assertThat(list.size()).isEqualTo(factorialUsingStreams(n, m));
     }
 
+    @Test
+    public void 백준_예제2() {
+        int n = 4;
+        int m = 2;
+        List<int[]> list = execute(n, m);
+        for (int[] ints : list) {
+            for (int i : ints) {
+                System.out.printf("%d ", i);
+            }
+            System.out.println();
+        }
+        Assertions.assertThat(list.size()).isEqualTo(factorialUsingStreams(n, m));
+    }
+
+    @Test
+    public void 숫자_한개만_있는_경우() {
+        int n = 1;
+        int m = 1;
+        List<int[]> list = execute(n, m);
+        for (int[] ints : list) {
+            for (int i : ints) {
+                System.out.printf("%d ", i);
+            }
+            System.out.println();
+        }
+        Assertions.assertThat(list.size()).isEqualTo(factorialUsingStreams(n, m));
+    }
+
+    @Test
+    public void 여덟개를_선택한_경우() {
+        int n = 8;
+        int m = 8;
+        List<int[]> list = execute(n, m);
+        System.out.println("list.size() = " + list.size());
+        Assertions.assertThat(list.size()).isEqualTo(factorialUsingStreams(n, m));
+    }
+
+    public int factorialUsingStreams(int n, int m) {
+        return IntStream.rangeClosed(n-m+1, n)
+                .reduce(1, (int x, int y) -> x * y);
+    }
+
+    List<int[]> execute(int n, int m) {
+        List<int[]> list = new ArrayList<>();
+        backtracking(list, n, m, 0, new int[m], new boolean[n+1]);
+        return list;
+    }
+
+    private void backtracking(List<int[]> list, int n, int m, int depth, int[] stack, boolean[] visit) {
+        if (depth >= m) {
+            list.add(stack.clone());
+            return;
+        }
+        for (int i = 1; i <=n ; i++) {
+            if(visit[i]) continue;
+
+            stack[depth] = i;
+            visit[i] = true;
+            backtracking(list, n, m, depth + 1, stack, visit);
+            visit[i] = false;
+        }
+    }
 }
--- src/test/java/algo/ExSimulation.java
+++ src/test/java/algo/ExSimulation.java
@@ -1,39 +1,59 @@
 package algo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-// 백준 https://www.acmicpc.net/problem/14503
-// 1 아이디어
-//   while 문으로 특정조건 종료될때까지 반복
-//   4방향을 for문으로 탐색
-//   더이상 탐색 불가능할 경우, 뒤로 한칸 후진
-//   후진이 불가능 하면 종료
-// 2 시간 복잡도 : N*M*4 -> 2억을 초과하는지
-// 3 자료구조 :
-// 전체 지도 int[][],
-// 로봇청소기 위치 방향 청소한 곳 수
+/**
+    - 주어진 조건을 되도록 그대로 구현(나중에 매우 헷갈림)
+    - 되도록 쉽게 구현
+        > 최악 케이스 : 코드 복잡해 디버깅 안됨, 시간 대부분 소모
+
+ **** 중요: 문제에서 로직의 순서 및 내용을 정확히 인지해야함
+
+ */
+
+/** 백준 https://www.acmicpc.net/problem/14503
+ N : row 세로
+ M : column 가로
+ (3 ≤ N, M ≤ 50)
+ d : 방향 0=북, 1=동, 2=남, 3=서
+
+ (row, column, 벽여부) 1=벽, 0=빈칸
+
+ 1 아이디어
+   while 문으로 특정조건 종료될때까지 반복
+   4방향을 for문으로 탐색
+   더이상 탐색 불가능할 경우, 뒤로 한칸 후진
+   후진이 불가능 하면 종료
+ 2 시간 복잡도 : N*M*4 -> 2억을 초과하는지
+ 3 자료구조 :
+ 전체 지도 int[][],
+ 로봇청소기 위치 방향 청소한 곳 수
+*/
 public class ExSimulation {
-    int n;
-    int m;
-
-    int y;
-    int x;
-    int d;
-    int[][] map;
-
-    int count=0;
-
-    int[] dy = new int[]{-1, 0, 1, 0};
-    int[] dx = new int[]{0, -1, 0, 1};
 
     @Test
-    public void test() {
-        n = 11;
-        m = 10;
-        y = 7;
-        x = 4;
-        d = 0;
-        map = new int[][]{
+    public void 백준_예제1() {
+        int n = 3; // rowCount
+        int m = 3; // columnCount
+        int[] robotPosition = {1, 1, 0};
+        int[][] map = {
+                {1, 1, 1},
+                {1, 0, 1},
+                {1, 1, 1}
+        };
+        int cleanSectionCount = simulation(map, robotPosition);
+        Assertions.assertThat(cleanSectionCount).isEqualTo(1);
+
+        System.out.println(cleanSectionCount);
+    }
+
+    @Test
+    public void 백준_예제2() {
+        int n = 11; // rowCount
+        int m = 10; // columnCount
+        int[] robotPosition = {7, 4, 0};
+        int[][] map = {
                 {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
                 {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
                 {1, 0, 0, 0, 1, 1, 1, 1, 0, 1},
@@ -46,39 +66,105 @@ public class ExSimulation {
                 {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
                 {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
         };
-        simulation();
+        int cleanSectionCount = simulation(map, robotPosition);
+        Assertions.assertThat(cleanSectionCount).isEqualTo(57);
+
+        System.out.println(cleanSectionCount);
     }
 
-    private void simulation() {
+    @Test
+    public void 예제3() {
+        int n = 5; // rowCount
+        int m = 5; // columnCount
+        int[] robotPosition = {2, 2, 1}; // 초기 위치 (2, 2)에서 동쪽(1) 방향
+        int[][] map = {
+                {1, 1, 1, 1, 1},
+                {1, 0, 0, 0, 1},
+                {1, 0, 0, 0, 1},
+                {1, 0, 0, 0, 1},
+                {1, 1, 1, 1, 1}
+        };
+        int cleanSectionCount = simulation(map, robotPosition);
+        Assertions.assertThat(cleanSectionCount).isEqualTo(9);
+    }
+
+    @Test
+    public void 예제4() {
+        int n = 7; // rowCount
+        int m = 7; // columnCount
+        int[] robotPosition = {3, 3, 2}; // 초기 위치 (3, 3)에서 남쪽(2) 방향
+        int[][] map = {
+                {1, 1, 1, 1, 1, 1, 1},
+                {1, 0, 0, 0, 0, 0, 1},
+                {1, 0, 1, 1, 1, 0, 1},
+                {1, 0, 1, 0, 1, 0, 1},
+                {1, 0, 1, 1, 1, 0, 1},
+                {1, 0, 0, 0, 0, 0, 1},
+                {1, 1, 1, 1, 1, 1, 1}
+        };
+        int cleanSectionCount = simulation(map, robotPosition);
+        Assertions.assertThat(cleanSectionCount).isEqualTo(1);
+    }
+
+    @Test
+    public void 예제5() {
+        int n = 10; // rowCount
+        int m = 10; // columnCount
+        int[] robotPosition = {1, 1, 3}; // 초기 위치 (1, 1)에서 서쪽(3) 방향
+        int[][] map = {
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 1, 1, 1, 1, 1, 1, 0, 1},
+                {1, 0, 1, 0, 0, 0, 0, 1, 0, 1},
+                {1, 0, 1, 0, 1, 1, 0, 1, 0, 1},
+                {1, 0, 1, 0, 0, 0, 0, 1, 0, 1},
+                {1, 0, 1, 1, 1, 1, 1, 1, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+        };
+        int cleanSectionCount = simulation(map, robotPosition);
+        Assertions.assertThat(cleanSectionCount).isEqualTo(26);
+
+        System.out.println(cleanSectionCount);
+    }
+
+    // 북, 동, 남, 서
+    final private int[] rowDirection = {-1, 0, 1, 0};
+    final private int[] columnDirection = {0, 1, 0, -1};
+
+    private int simulation(int[][] map, int[] robotPosition) {
+        int count = 0;
+        int robotRow = robotPosition[0];
+        int robotColumn = robotPosition[1];
+        int robotDirection = robotPosition[2];
         while (true) {
-            if(map[y][x] == 0) {
-                map[y][x] = 2;
+            if(map[robotRow][robotColumn] == 0) {
+                map[robotRow][robotColumn] = 2;
                 count++;
             }
-            boolean findNotClean = false;
+            boolean isMovedNext = false;
             for (int i = 0; i < 4; i++) {
-                int nextD = (d + i + 1) % 4; // +1을 해야함 보던방향에서 바라보던 방향을 먼저 탐색하는것이 아닌 반시계 90을 먼저 돌기때문
-                int nextY = y + dy[nextD];
-                int nextX = x + dx[nextD];
-                if (map[nextY][nextX] == 0) { // nextY랑 nextX가 0보다 크고 각각 N, M보다 작아야 한다.
-                    y = nextY;
-                    x = nextX;
-                    d = nextD;
-                    findNotClean = true;
+                int nextDirection = (robotDirection +3 - i ) % 4; // 청소 구역이 있을 때, 현재 방향에서 다음 반시계 방향으로 먼저 회전함!!
+                int nextRow = robotRow + rowDirection[nextDirection];
+                int nextColumn = robotColumn + columnDirection[nextDirection];
+                if (  map[nextRow][nextColumn] == 0) {
+                    robotRow = nextRow;
+                    robotColumn = nextColumn;
+                    robotDirection = nextDirection;
+                    isMovedNext = true;
                     break;
                 }
             }
-            if (!findNotClean) {
-                int nextY = y - dy[d];
-                int nextX = x - dx[d];
-                if (map[nextY][nextX] == 1) {
-                    break;
-                } else {
-                    y = nextY;
-                    x = nextX;
-                }
+            if(isMovedNext) continue;
+            int backRow = robotRow - rowDirection[robotDirection];
+            int backColumn = robotColumn - columnDirection[robotDirection];
+            if (map[backRow][backColumn] == 2) {
+                robotRow = backRow;
+                robotColumn = backColumn;
+            } else {
+                break;
             }
         }
-        System.out.println("count = " + count);
+        return count;
     }
 }
