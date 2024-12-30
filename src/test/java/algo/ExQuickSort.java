c64c1ebb8d0a2bc1952eeb1af32ae30b59de60c0
summy1105 <73230224+summy1105@users.noreply.github.com>
2024-12-31 00:03:22 +0900

    quick sort복습

--- src/test/java/algo/ExQuickSort.java
+++ src/test/java/algo/ExQuickSort.java
@@ -1,52 +1,57 @@
 package algo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class ExQuickSort {
-    static int[] nList;
 
     @Test
     public void test() {
-        nList = new int[]{4, 1, 5, 2, 3};
+        int n = 100;
+        List<Integer> collect = IntStream.rangeClosed(1, n)
+                .boxed()
+                .collect(Collectors.toList());
 
-        // 외우기
-        quickSort(nList, 0, nList.length-1);
+        Collections.shuffle(collect);
 
-        for (int i : nList) {
-            System.out.print(i+" ");
-        }
+        int[] list = collect.stream().mapToInt(Integer::intValue).toArray();
+
+        quickSort(list, 0, list.length-1);
+
+        Assertions.assertThat(list).containsExactly(IntStream.rangeClosed(1, n).toArray());
     }
 
     private void quickSort(int[] list, int left, int right) {
-        if(left >= right) return;
+        if (left >= right) return;
 
-        int part = partition(list, left, right);
+        int partBasicIdx = partition(list, left, right);
 
-        quickSort(list, left, part - 1);
-        quickSort(list, part + 1, right);
+        quickSort(list, left, partBasicIdx - 1);
+        quickSort(list, partBasicIdx + 1, right);
     }
 
-    private int partition(int[] list, int left, int right) {
-        int mid = (left + right) / 2;
-        swap(list, mid, right); // pivot right 선택
+    private int partition(int[] list, int leftCur, int rightCur) {
+        int pivotIdx = rightCur;
+        int pivotNum = list[pivotIdx];
 
-        int pivot = list[right];
-
-        int lCur = left;
-        int rCur = right;
-        while (lCur < rCur) {
-            while(pivot > list[lCur]) lCur++;
-            while(lCur<rCur && pivot <= list[rCur]) rCur--; // pivot equal 조건은 right cursor랑
-            swap(list, lCur, rCur);
+        while (leftCur < rightCur) {
+            while (list[leftCur] < pivotNum) leftCur++;
+            while (leftCur < rightCur && pivotNum <= list[rightCur]) rightCur--;
+            swap(list, leftCur, rightCur);
         }
-        list[right] = list[rCur]; // right cursor랑 right랑 swap
-        list[rCur] = pivot;
-        return rCur;
+        list[pivotIdx] = list[rightCur];
+        list[rightCur] = pivotNum;
+        return rightCur;
     }
 
-    private void swap(int[] list, int aPoint, int bPoint) {
-        int temp = list[aPoint];
-        list[aPoint] = list[bPoint];
-        list[bPoint] = temp;
+    private void swap(int[] list, int aIdx, int bIdx) {
+        int temp = list[aIdx];
+        list[aIdx] = list[bIdx];
+        list[bIdx] = temp;
     }
 }
